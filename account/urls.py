0c25e9a333b7f5411a770cd18291326e9cf89643
Beknaz <choybekov.beknaz@gmail.com>
2022-08-09 12:29:21 +0600

    finish

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterAPIView, activate
+from .views import RegisterAPIView
 
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -7,7 +7,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view()),
-    path('activate/<str:activation_code>/', activate),
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
