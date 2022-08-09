a02094f41ddc9ef919bbdc7dec93d6bb953b7c76
myrzaiym00 <myrzaiym.00.01@gmail.com>
2022-08-09 14:54:26 +0600

    update

--- account/views.py
+++ account/views.py
@@ -1,14 +1,8 @@
-from django.shortcuts import redirect
-from django.contrib.auth import get_user_model
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework.generics import get_object_or_404
 from drf_yasg.utils import swagger_auto_schema
 
 from .serializers import RegisterSerializer
-
-User = get_user_model()
 
 class RegisterAPIView(APIView):
     @swagger_auto_schema(request_body=RegisterSerializer)
