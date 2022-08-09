13abed281da7b33d404f85654de8baa05c1beddc
gulsana <aidaadarchi5@gmail.com>
2022-08-09 11:06:32 +0600

    post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} => {self.title}"
