2911e849b329583ed6000402c2a3c547a31dfa62
Beknaz <choybekov.beknaz@gmail.com>
2022-08-09 11:18:51 +0600

    CommentAndLike

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,11 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     def __str__(self):
         return f"Comment{self.user.username} -> {self.post.title} [{self.created_at}]"
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    def __str__(self):
+        return f"Like{self.user.username} -> {self.post.title} [{self.created_at}]"
+        
